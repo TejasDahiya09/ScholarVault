3980327d35618ae392f88ab10263b96e58816e40
Tejas Dahiya <dahiyatejas35@gmail.com>
2025-12-24 23:18:21 +0530

    Fix duplicate import and router declaration in bookmarks.js

--- backend/src/routes/bookmarks.js
+++ backend/src/routes/bookmarks.js
@@ -33,10 +33,5 @@ router.get("/details", authenticate, async (req, res, next) => {
 });
 
 export default router;
-import { Router } from "express";
-import bookmarksDB from "../db/bookmarks.js";
-import { authenticate } from "../middlewares/auth.js";
-
-const router = Router();
 
 // Bookmarks routes removed
